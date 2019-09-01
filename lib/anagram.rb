3bd0539e6f514195289e1a7d7bfcfd934d3301d2
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-01 22:56:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,10 +7,9 @@ class Anagram
       @word = word
       self
   end
-   # select | |
-   # size after 
+
   def match(words)
-    look = words.select do |find| 
+   words.select do |find| 
       find.split("").sort == @word.split("").sort
       
     #binding.pry
